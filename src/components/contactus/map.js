45398f5ec2e10c3b4930b3e077ea66caf33a182d
Ramivasudev <90176310+Ramivasudev@users.noreply.github.com>
2024-04-08 12:57:46 +0530

    Update map.js

--- src/components/contactus/map.js
+++ src/components/contactus/map.js
@@ -1,31 +1,20 @@
 import React from "react";
-import GoogleMapReact from 'google-map-react';
-
-const AnyReactComponent = () => <div>MAP</div>;
 
 export default function SimpleMap(){
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
 
   return (
-    // Important! Always set the container height explicitly
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-        />
-      </GoogleMapReact>
-    </div>
+
+              <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3685.0344244499056!2d72.93065397507668!3d22.54038317951472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fc8bb9aa097e7%3A0xf8d6b2b78eabe72b!2sTECH%20ELECON%20PVT%20LTD!5e0!3m2!1sen!2sin!4v1712555196827!5m2!1sen!2sin"
+        width="100%"
+        height="600vh"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        title="Google Map"
+      />
+
+
   );
 }
